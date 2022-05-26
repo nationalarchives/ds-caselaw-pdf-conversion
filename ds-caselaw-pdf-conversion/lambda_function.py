e40ba2b1c4aecc92d13b6caa8c27023190d073a4
James Smith <james@floppy.org.uk>
2022-05-26 13:49:40 +0100

    simplify libreoffice extraction
    seems to work OK in terms of invoking the program; something else might be wrong in terms of paths though.

--- ds-caselaw-pdf-conversion/lambda_function.py
+++ ds-caselaw-pdf-conversion/lambda_function.py
@@ -1,48 +1,25 @@
-import os
 import subprocess
 import tarfile
-from io import BytesIO
 import glob
 import boto3
 
-libre_office_install_dir = "/tmp/instdir"
-print(glob.glob("/opt/*"))
 
-# open file
-
-file = tarfile.open("/opt/lo.tar.gz")
-# extracting file
-file.extractall(libre_office_install_dir)
-file.close()
-
-def new_load_libre_office():
-    libre_office_install_dir = "/tmp/instdir"
-    print(glob.glob("/opt/*"))
-
+def extract_libre_office():
     # open file
-
     file = tarfile.open("/opt/lo.tar.gz")
-    # extracting file
+    # extract file
     file.extractall('/tmp')
     file.close()
-    subprocess.run([f"{libre_office_install_dir}/program/soffice.bin", "--help"])
+    return "/tmp/instdir"
 
-
-def load_libre_office():
-    if os.path.exists(libre_office_install_dir) and os.path.isdir(libre_office_install_dir):
-        print('We have a cached copy of LibreOffice, skipping extraction')
-    else:
-        print('No cached copy of LibreOffice, extracting tar stream from Brotli file.')
-        buffer = BytesIO()
-
-    print('Extracting tar stream to /tmp for caching.')
-    with tarfile.open(fileobj=buffer) as tar:
-        tar.extractall('/tmp')
-    print('Done caching LibreOffice!')
-    subprocess.run([f"{libre_office_install_dir}/program/soffice.bin", "--help"])
+def libre_office_executable():
+    libre_office_install_dir = extract_libre_office()
     return f"{libre_office_install_dir}/program/soffice.bin"
 
-
 def handler(event, context):
-    print("In Lambda")
-    new_load_libre_office()
+    lo = libre_office_executable()
+    output = subprocess.run([lo, "--help"], capture_output=True, text=True)
+    print("stdout:")
+    print(output.stdout)
+    print("stderr:")
+    print(output.stderr)
